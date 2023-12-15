4d0932d7968eec737b1ed76acc47f13b366c01f2
Roger Bloise <roger.bloise@wavy.global>
2023-12-14 23:55:51 -0300

    muda responses de render html para json puro

--- form/app.py
+++ form/app.py
@@ -1,62 +1,64 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, request,  make_response, jsonify
 import sqlite3 as sql
 
 app=Flask(__name__)
 
-@app.route("/")
-@app.route("/index")
-
-def index():
+@app.route("/events")
+def events():
     con = sql.connect("form_db.db")
     con.row_factory=sql.Row
     cur=con.cursor()
     cur.execute("select * from events")
     data=cur.fetchall()
-    return render_template ("index.html", datas=data)
+    return make_response(jsonify([dict(ix) for ix in data]))
 
-@app.route("/add_event", methods=["POST", "GET"])
+@app.route("/event/<string:id>", methods=["GET"])
+def get_event(id):
+    con = sql.connect("form_db.db")
+    con.row_factory=sql.Row
+    cur=con.cursor()
+    cur.execute("select * from events where id=?", (id))
+    data=cur.fetchall()
+    return make_response(jsonify([dict(ix) for ix in data]))
+
+@app.route("/add_event", methods=["POST"])
 def add_event():
-    if request.method=="POST":
-        usuario=request.form["usuario"]
-        nome_evento=request.form["nome_evento"]
-        local=request.form["local"]
-        data=request.form["data"]
-        con=sql.connect("form_db.db")
-        cur=con.cursor()
-        cur.execute("insert into events(USUARIO,NOME_EVENTO,LOCAL,DATA) values (?,?,?,?)", (usuario, nome_evento, local, data))
-        con.commit()
-        flash("Dados cadastrados", "success")
-        return redirect(url_for("index"))
-    return render_template("add_event.html")
+    form = request.json
+    usuario=form["usuario"]
+    nome_evento=form["nome_evento"]
+    local=form["local"]
+    data=form["data"]
+    con=sql.connect("form_db.db")
+    cur=con.cursor()
+    cur.execute("insert into events(USUARIO,NOME_EVENTO,LOCAL,DATA) values (?,?,?,?)", (usuario, nome_evento, local, data))
+    con.commit()
+    return make_response(jsonify(form))
 
-@app.route("/edit_event/<string:id>", methods=["POST","GET"])
+@app.route("/edit_event/<string:id>", methods=["PATCH"])
 def edit_event(id):
-    if request.method=="POST":
-        usuario=request.form["usuario"]
-        nome_evento=request.form["nome_evento"]
-        local=request.form["local"]
-        data=request.form["data"]
-        con=sql.connect("form_db.db")
-        cur=con.cursor()
-        cur.execute("update events set USUARIO=?,NOME_EVENTO=?,LOCAL=?,DATA=? where ID=?", (usuario,nome_evento,local,data,id))
-        con.commit()
-        flash("Dados atualizados", "success")
-        return redirect(url_for("index"))
+    form = request.json
+    usuario=form["usuario"]
+    nome_evento=form["nome_evento"]
+    local=form["local"]
+    data=form["data"]
+    con=sql.connect("form_db.db")
+    cur=con.cursor()
+    cur.execute("update events set USUARIO=?,NOME_EVENTO=?,LOCAL=?,DATA=? where ID=?", (usuario,nome_evento,local,data,id))
+    con.commit()
     con=sql.connect("form_db.db")
     con.row_factory=sql.Row
     cur=con.cursor()
     cur.execute("select * from events where ID =?", (id,))
-    data=cur.fetchone()
-    return render_template("edit_event.html", datas=data)
+    new_data=cur.fetchall()
+    return make_response(jsonify([dict(ix) for ix in new_data]))
 
-@app.route("/delete_event/<string:id>", methods=["GET"])
+@app.route("/delete_event/<string:id>", methods=["DELETE"])
 def delete_event(id):
     con=sql.connect("form_db.db")
     cur=con.cursor()
     cur.execute("delete from events where ID=?", (id,))
     con.commit()
-    flash("Dados deletados", "warning")
-    return redirect(url_for("index"))
+    return make_response('Data deleted')
 
 if __name__=='__main__':
     app.secret_key="admin123"
